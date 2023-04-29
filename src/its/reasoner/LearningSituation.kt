a4723225df1b00816589f9951c9740e239c954a5
Max-Person <max_person@mail.ru>
2023-04-29 18:52:41 +0300

    [feat] изменение конструкторов LearningSituation для работы с вынесенным в DomainModel общим рдф представлением

--- src/its/reasoner/LearningSituation.kt
+++ src/its/reasoner/LearningSituation.kt
@@ -1,5 +1,6 @@
 package its.reasoner
 
+import its.model.DomainModel
 import its.reasoner.util.JenaUtil
 import its.reasoner.util.RDFUtil.copy
 import org.apache.jena.rdf.model.Model
@@ -8,13 +9,15 @@ import org.apache.jena.riot.RDFDataMgr
 
 class LearningSituation (
     val model : Model,
-    val decisionTreeVariables : MutableMap<String, String> = mutableMapOf(),
+    val decisionTreeVariables : MutableMap<String, String>,
 ) {
 
-    constructor(filename: String) : this(ModelFactory.createDefaultModel().read(RDFDataMgr.open(filename), null)){
+    constructor(model : Model) : this(model, mutableMapOf()){
         val p = model.getProperty(JenaUtil.genLink(JenaUtil.POAS_PREF, JenaUtil.DECISION_TREE_VAR_PREDICATE))
         decisionTreeVariables.putAll(model.listSubjectsWithProperty(p).toList().map{res -> res.getProperty(p).`object`.asLiteral().string to res.localName}.toMap())
     }
+
+    constructor(filename: String) : this(ModelFactory.createDefaultModel().read(RDFDataMgr.open(filename), null, "TTL").add(DomainModel.domainRDF))
 
     fun copy(model: Model = this.model.copy(), decisionTreeVariables: MutableMap<String, String> = this.decisionTreeVariables.toMutableMap()) : LearningSituation {
         return LearningSituation(model, decisionTreeVariables)
